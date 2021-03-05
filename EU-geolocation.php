c1ab9df46c233a9c8a00dc953907d1764eaf4286
Canspan BMG <web@canspanclients.com>
2021-03-05 09:51:48 -0500

    Update EU-geolocation.php

--- EU-geolocation.php
+++ EU-geolocation.php
@@ -1,22 +1,38 @@
-/*
-Plugin Name: John Gamboa's GeoTarget Redirect Example
-Description: Redirecting visitor traffic based on geo-location using the WPEngine GeoTarget feature
-Version: 1.2
-License: GPLv2
-*/
+function geoIpRedirect() : void
+{
+ $redirectCookieName = 'sytx_geo_redirect';
+ try {
+ // Exit if its not the homepage or
+ // the redirect already happened or
+ // NGINX didnt provide the environment variable
+ if (isset($_COOKIE[$redirectCookieName]) ||
+ is_front_page() === false ||
+ empty($country = strtoupper(trim(getenv('HTTP_GEOIP_COUNTRY_CODE'))))) {
+ return;
+ }
 
-function country_geo_redirect() {
+ // Set the cookie!
+ setcookie($redirectCookieName, time(), 0);
 
-$country = getenv('HTTP_GEOIP_COUNTRY_CODE');
-
-if ( $continent == 'EU' ) {
-
-wp_redirect( 'https://www.syntax.com/de-de', 301 );
-
-     exit;
-
-} 
-
+ switch (true) {
+ // Redirect for German site
+ case in_array($country, [
+ 'AT', 'BE', 'BG', 'HR',
+ 'CY', 'CZ', 'DK', 'EE',
+ 'FI', 'FR', 'DE', 'GR',
+ 'HU', 'IE', 'IT', 'LV',
+ 'LT', 'LU', 'MT', 'NL',
+ 'PL', 'PT', 'RO', 'SK',
+ 'SI', 'ES', 'SE',
+ ]) : {
+ header(sprintf(... [
+ 'Location: %s/de-de',
+ site_url()
+ ]));
+ die;
+ }
+ }
+ } catch (\Throwable $exception) {
+ // Silence is golden
+ }
 }
-
-add_action('init', 'country_geo_redirect');
